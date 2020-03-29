a85a574f1c04eccafef7a82e686bfc9b5f4c5ac2
Argin Simonian <arginsimon@gmail.com>
2020-03-28 22:29:43 -0500

    8

--- server.js
+++ server.js
@@ -1,38 +1,9 @@
-const express = require('express');
-const path = require('path');
-
+const path = require("path");
+const express = require("express");
 const app = express();
-
-// Init Middleware
-// app.use(express.json({ extended: false }));
-
-// Define Routes
-app.use('/app/app.component', require('./src/app/app.component'));
-/* app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts')); */
-
-// Serve static assets in production
-//if (process.env.NODE_ENV === 'production') {
-// Set static folder
-app.use(express.static('src/app'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
-});
-// }
-
-const PORT = process.env.PORT || 5003;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// Serve only the static files form the dist directory
-/* app.use(express.static(__dirname + '/src/app'));
-
+app.use(express.static(__dirname + '/dist/frontEnd'));
 app.get('/*', function(req, res) {
-
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname, 'src', 'index.html'))
 });
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080); */
+app.listen(process.env.PORT || 8080);
