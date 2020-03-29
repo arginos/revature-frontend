2227a57c706664f531289a684f7e80c5f817feb5
Argin Simonian <arginsimon@gmail.com>
2020-03-28 22:30:36 -0500

    9

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist/frontEnd'));
+app.use(express.static(__dirname + '/src'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'src', 'index.html'))
 });
